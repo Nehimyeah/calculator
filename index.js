f1365b3f20ef65e2f96353f8f50c4eda2700fb86
Nehemiah Amanuel <nehemiaha1@outlook.com>
2021-06-27 12:52:01 +0300

    Create an operate function

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const divide = (a, b) => {
 }
 
 const numbers = document.querySelectorAll('#number');
-
 const ds = document.querySelector('#ds');
+
 
 numbers.forEach((button) => {
     button.addEventListener('click', (e) => {
@@ -29,4 +29,19 @@ numbers.forEach((button) => {
     })
 })
 
-
+const operate = (operation, a, b) => {
+    switch(operation) {
+        case '+':
+            add(a, b);
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case 'x':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+    }
+}
